f7b8907622cc0cecf02fc527edf8f4f781a29ae3
ijl <ijl@mailbox.org>
2018-12-11 23:40:24 +0000

    loads() benchmarks use unicode

--- bench/util.py
+++ bench/util.py
@@ -11,10 +11,10 @@ dirname = os.path.join(os.path.dirname(__file__), '../data')
 def read_fixture_str(filename):
     if filename.endswith('.xz'):
         with lzma.open(os.path.join(dirname, filename), 'r') as fileh:
-            return fileh.read()
+            return fileh.read().decode('utf-8')
     else:
         with open(os.path.join(dirname, filename), 'r') as fileh:
-            return fileh.read()
+            return fileh.read().decode('utf-8')
 
 
 def read_fixture_obj(filename):
