6bddb2edfb5e2317aeb774a5469573dd826dadfd
ijl <ijl@mailbox.org>
2019-01-21 18:50:07 +0000

    Fix rapidjson.loads() benchmarks

--- bench/benchmark_loads.py
+++ bench/benchmark_loads.py
@@ -2,10 +2,10 @@
 
 import unittest
 
-import json
-import orjson
-import rapidjson
-import ujson
+from json import loads as json_loads
+from orjson import loads as orjson_loads
+from rapidjson import loads as rapidjson_loads
+from ujson import loads as ujson_loads
 
 from .util import read_fixture_obj, read_fixture_str
 
@@ -14,94 +14,94 @@ def test_loads_canada_orjson(benchmark):
     benchmark.group = 'canada.json deserialization'
     benchmark.extra_info['lib'] = 'orjson'
     data = read_fixture_str("canada.json.xz")
-    benchmark(orjson.loads, data)
+    benchmark(orjson_loads, data)
 
 def test_loads_canada_ujson(benchmark):
     benchmark.group = 'canada.json deserialization'
     benchmark.extra_info['lib'] = 'ujson'
     data = read_fixture_str("canada.json.xz")
-    benchmark(ujson.loads, data)
+    benchmark(ujson_loads, data)
 
 def test_loads_canada_json(benchmark):
     benchmark.group = 'canada.json deserialization'
     benchmark.extra_info['lib'] = 'json'
     data = read_fixture_str("canada.json.xz")
-    benchmark(json.loads, data)
+    benchmark(json_loads, data)
 
 def test_loads_canada_rapidjson(benchmark):
     benchmark.group = 'canada.json deserialization'
     benchmark.extra_info['lib'] = 'rapidjson'
     data = read_fixture_str("canada.json.xz")
-    benchmark(json.loads, data)
+    benchmark(rapidjson_loads, data)
 
 def test_loads_citm_catalog_orjson(benchmark):
     benchmark.group = 'citm_catalog.json deserialization'
     benchmark.extra_info['lib'] = 'orjson'
     data = read_fixture_str("citm_catalog.json.xz")
-    benchmark(orjson.loads, data)
+    benchmark(orjson_loads, data)
 
 def test_loads_citm_catalog_ujson(benchmark):
     benchmark.group = 'citm_catalog.json deserialization'
     benchmark.extra_info['lib'] = 'ujson'
     data = read_fixture_str("citm_catalog.json.xz")
-    benchmark(ujson.loads, data)
+    benchmark(ujson_loads, data)
 
 def test_loads_citm_catalog_json(benchmark):
     benchmark.group = 'citm_catalog.json deserialization'
     benchmark.extra_info['lib'] = 'json'
     data = read_fixture_str("citm_catalog.json.xz")
-    benchmark(json.loads, data)
+    benchmark(json_loads, data)
 
 def test_loads_citm_catalog_rapidjson(benchmark):
     benchmark.group = 'citm_catalog.json deserialization'
     benchmark.extra_info['lib'] = 'rapidjson'
     data = read_fixture_str("citm_catalog.json.xz")
-    benchmark(json.loads, data)
+    benchmark(rapidjson_loads, data)
 
 def test_loads_github_orjson(benchmark):
     benchmark.group = 'github.json deserialization'
     benchmark.extra_info['lib'] = 'orjson'
     data = read_fixture_str("github.json.xz")
-    benchmark(orjson.loads, data)
+    benchmark(orjson_loads, data)
 
 def test_loads_github_ujson(benchmark):
     benchmark.group = 'github.json deserialization'
     benchmark.extra_info['lib'] = 'ujson'
     data = read_fixture_str("github.json.xz")
-    benchmark(ujson.loads, data)
+    benchmark(ujson_loads, data)
 
 def test_loads_github_json(benchmark):
     benchmark.group = 'github.json deserialization'
     benchmark.extra_info['lib'] = 'json'
     data = read_fixture_str("github.json.xz")
-    benchmark(json.loads, data)
+    benchmark(json_loads, data)
 
 def test_loads_github_rapidjson(benchmark):
     benchmark.group = 'github.json deserialization'
     benchmark.extra_info['lib'] = 'rapidjson'
     data = read_fixture_str("github.json.xz")
-    benchmark(json.loads, data)
+    benchmark(rapidjson_loads, data)
 
 def test_loads_twitter_orjson(benchmark):
     benchmark.group = 'twitter.json deserialization'
     benchmark.extra_info['lib'] = 'orjson'
     data = read_fixture_str("twitter.json.xz")
-    benchmark(orjson.loads, data)
+    benchmark(orjson_loads, data)
 
 def test_loads_twitter_ujson(benchmark):
     benchmark.group = 'twitter.json deserialization'
     benchmark.extra_info['lib'] = 'ujson'
     data = read_fixture_str("twitter.json.xz")
-    benchmark(ujson.loads, data)
+    benchmark(ujson_loads, data)
 
 def test_loads_twitter_json(benchmark):
     benchmark.group = 'twitter.json deserialization'
     benchmark.extra_info['lib'] = 'json'
     data = read_fixture_str("twitter.json.xz")
-    benchmark(json.loads, data)
+    benchmark(json_loads, data)
 
 def test_loads_twitter_rapidjson(benchmark):
     benchmark.group = 'twitter.json deserialization'
     benchmark.extra_info['lib'] = 'rapidjson'
     data = read_fixture_str("twitter.json.xz")
-    benchmark(json.loads, data)
+    benchmark(rapidjson_loads, data)
