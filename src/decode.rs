8f310ca9680ea5aa51300e8d69ee22abb884df21
ijl <ijl@mailbox.org>
2019-02-19 14:57:36 +0000

    Platform-specific signed/unsigned int libpython funcs

--- src/decode.rs
+++ src/decode.rs
@@ -102,14 +102,28 @@ impl<'de, 'a> Visitor<'de> for JsonValue {
     where
         E: de::Error,
     {
-        Ok(unsafe { pyo3::ffi::PyLong_FromLongLong(value) })
+        #[cfg(target_os = "windows")]
+        {
+            Ok(unsafe { pyo3::ffi::PyLong_FromLongLong(value) })
+        }
+        #[cfg(not(target_os = "windows"))]
+        {
+            Ok(unsafe { pyo3::ffi::PyLong_FromLong(value) })
+        }
     }
 
     fn visit_u64<E>(self, value: u64) -> Result<Self::Value, E>
     where
         E: de::Error,
     {
-        Ok(unsafe { pyo3::ffi::PyLong_FromLongLong(value as i64) })
+        #[cfg(target_os = "windows")]
+        {
+            Ok(unsafe { pyo3::ffi::PyLong_FromUnsignedLongLong(value) })
+        }
+        #[cfg(not(target_os = "windows"))]
+        {
+            Ok(unsafe { pyo3::ffi::PyLong_FromUnsignedLong(value) })
+        }
     }
 
     fn visit_f64<E>(self, value: f64) -> Result<Self::Value, E>
