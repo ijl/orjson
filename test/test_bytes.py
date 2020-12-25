fcab25c798a6a8d0e444e050a7e2d075abd48989
Kevin Chen <kevin@bloxroute.com>
2020-12-25 01:23:57 -0600

    Add unit test for nested values

--- test/test_bytes.py
+++ test/test_bytes.py
@@ -15,3 +15,14 @@ class BytesTests(unittest.TestCase):
         obj = b'{"a":"a","b":1}'
         result = orjson.dumps(obj, option=orjson.OPT_DIRECT_SERIALIZE_BYTES)
         self.assertEqual(b'{"a":"a","b":1}', result)
+
+    def test_bytes_in_dict(self):
+        value = {"a": "a", "b": 1}
+        serialized_value = orjson.dumps(value)
+
+        obj = {"foo": serialized_value}
+        matching_obj = {"foo": value}
+
+        self.assertEqual(
+            orjson.dumps(matching_obj), orjson.dumps(obj, option=orjson.OPT_DIRECT_SERIALIZE_BYTES)
+        )
