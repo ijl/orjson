853ffbdf8dc5f34792765c22aa835e1b67d90a76
Aarni Koskela <akx@iki.fi>
2022-01-20 15:34:55 +0200

    fix(errors): adjust column offset if not at char boundary
    Fixes #226
    
    See https://www.reddit.com/r/rust/comments/765n76/psa_caution_with_slicing_strings/

---
+++ test/test_issue221.py
@@ -0,0 +1,11 @@
+import pytest
+import orjson
+
+
+@pytest.mark.parametrize("input", [
+    b'"\xc8\x93',
+    b'"\xc8',
+])
+def test_invalid(input):
+    with pytest.raises(orjson.JSONDecodeError):
+        orjson.loads(input)
