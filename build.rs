3dc105aa57cc25e971e71b59866039728ca5f721
messense <messense@icloud.com>
2022-02-09 18:43:17 +0800

    Use pyo3-build-config

--- build.rs
+++ build.rs
@@ -1,13 +1,3 @@
-use std::process::Command;
-
 fn main() {
-    let out = Command::new("python")
-        .args(&["-c", "import sys; print(sys.version_info[1])"])
-        .output()
-        .expect("python version did not print");
-    let version = u8::from_str_radix(String::from_utf8_lossy(&out.stdout).trim(), 10)
-        .expect("python version was not parsed");
-    for each in 8..(version + 1) {
-        println!("cargo:rustc-cfg=Py_3_{}", each);
-    }
+    pyo3_build_config::use_pyo3_cfgs();
 }
