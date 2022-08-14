08971360687b01058e5630c8d417a69a67b1011d
ijl <ijl@mailbox.org>
2022-08-14 15:34:42 +0000

    pyproject.toml metadata for maturin 0.13.2 and PEP 621, cargo update

--- include/json/src/lib.rs
+++ include/json/src/lib.rs
@@ -300,7 +300,7 @@
 //! [macro]: https://docs.serde.rs/serde_json/macro.json.html
 //! [`serde-json-core`]: https://github.com/rust-embedded-community/serde-json-core
 
-#![doc(html_root_url = "https://docs.rs/serde_json/1.0.82")]
+#![doc(html_root_url = "https://docs.rs/serde_json/1.0.83")]
 // Ignored clippy lints
 #![allow(
     clippy::collapsible_else_if,
