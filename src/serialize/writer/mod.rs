58a73f360932c8b585b48681735b2fb432ae1354
ijl <ijl@mailbox.org>
2024-04-15 21:30:19 +0000

    format_escaped_str() more unified LUT implementation

--- src/serialize/writer/mod.rs
+++ src/serialize/writer/mod.rs
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 mod byteswriter;
-#[cfg(not(feature = "unstable-simd"))]
 mod escape;
 mod formatter;
 mod json;
-#[cfg(feature = "unstable-simd")]
-mod simd;
 
 pub use byteswriter::{BytesWriter, WriteExt};
 pub use json::{to_writer, to_writer_pretty};
